dea139ea719b27014829d1c4b72a861dcdd7ba00
rafazamp <rafaelprimieri@hotmail.com>
2023-04-07 15:34:13 -0300

    Exercício 05

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export function useForm(steps) {
+  const [passoAtual, setPassoAtual] = useState(0);
+
+  function mudarPasso(i, e) {
+    e.preventDefault();
+
+    if (i < 0 || i >= steps.length) {
+      return;
+    }
+
+    setPassoAtual(i);
+  }
+
+  return {
+    passoAtual,
+    componenteAtual: steps[passoAtual],
+    mudarPasso,
+    ehUltimoPasso: passoAtual + 1 == steps.length ? true : false,
+  };
+}
